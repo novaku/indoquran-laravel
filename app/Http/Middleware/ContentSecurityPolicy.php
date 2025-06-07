093167501cdc76648a5692adf9c950b072318a62
Nova Herdi Kusumah <novaherdi@gmail.com>
2025-06-07 19:53:16 +0700

    Fix: Update Content Security Policy and .htaccess for font handling; add comprehensive font fix script

--- app/Http/Middleware/ContentSecurityPolicy.php
+++ app/Http/Middleware/ContentSecurityPolicy.php
@@ -23,7 +23,7 @@ class ContentSecurityPolicy
                 "default-src 'self'",
                 "script-src 'self' 'unsafe-inline' 'unsafe-eval' blob: data: http://localhost:5173 ws://localhost:5173 https://*.google-analytics.com https://www.google-analytics.com",
                 "style-src 'self' 'unsafe-inline' blob: data: http://localhost:5173 https://fonts.bunny.net https://fonts.googleapis.com fonts.bunny.net",
-                "font-src 'self' data: blob: https://fonts.bunny.net https://fonts.gstatic.com fonts.bunny.net fonts.gstatic.com",
+                "font-src 'self' data: blob: https://fonts.bunny.net https://fonts.gstatic.com fonts.bunny.net fonts.gstatic.com https://*.indoquran.web.id my.indoquran.web.id https://my.indoquran.web.id",
                 "img-src 'self' data: blob: https://*.google-analytics.com https://www.google-analytics.com",
                 "media-src 'self' https://*.nos.wjv-1.neo.id https://*.equran.id https://*.equran.nos.wjv-1.neo.id https://*.quranicaudio.com https://*.qurancdn.com https://*.vercel.app *",
                 "connect-src 'self' ws://localhost:5173 http://localhost:5173 https://download.quranicaudio.com https://nominatim.openstreetmap.org https://*.google-analytics.com https://www.google-analytics.com",
