d3f859a2069f368932157f85893eeea7597ac298
Nova Herdi Kusumah <novaherdi@gmail.com>
2026-01-02 10:55:22 +0700

    update composer bash script

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -19,4 +19,4 @@ if (PHP_VERSION_ID < 50600) {
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInit88970a0117c062eed55fa8728fc43833::getLoader();
+return ComposerAutoloaderInitaeab9ec32efa47f5af79f46513f39975::getLoader();
