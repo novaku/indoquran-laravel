9b32940ac3c9a57281df83c4c7a351e9169dc6fa
Nova Herdi Kusumah <novaherdi@gmail.com>
2026-09-02 05:28:42 +0700

    chore: update manifest and build assets for production deployment, disable auto dark mode in chrome

--- resources/views/react.blade.php
+++ resources/views/react.blade.php
@@ -5,7 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="theme-color" content="#22c55e">
-    <meta name="color-scheme" content="light">
+    <meta name="color-scheme" content="only light">
+    <meta name="supported-color-schemes" content="light">
     
     <!-- Permissions Policy for Geolocation -->
     <meta http-equiv="Permissions-Policy" content="geolocation=(self)">
